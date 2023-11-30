4dd859327b70a5ff7bc676b98433d87fd8136345
Edwin Alejandro Galicia de la Quadra <edwin.galicia@agape.edu.sv>
2023-11-30 11:38:48 -0600

    agregar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using ElHogarLibros.ClienteBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://www.backend.com/api/") });
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
